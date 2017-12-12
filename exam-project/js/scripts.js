83bb456f77506427cb0aa51cade8e586de01f777
abolzz <abolzzy@gmail.com>
2017-12-12 21:37:42 +0100

    I am done with this

--- exam-project/js/scripts.js
+++ exam-project/js/scripts.js
@@ -15,15 +15,40 @@
 		}
 	});
 
-// When the user scrolls down, show dot navigation
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
-        document.getElementById("cd-vertical-nav").style.display = "block";
-    } else {
-        document.getElementById("cd-vertical-nav").style.display = "none";
-    }
-}
-
 		})(jQuery); // End of use strict
+
+$('#contact-form').bootstrapValidator({
+//        live: 'disabled',
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            Name: {
+                validators: {
+                    notEmpty: {
+                        message: 'The Name is required and cannot be empty'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required'
+                    },
+                    emailAddress: {
+                        message: 'The email address is not valid'
+                    }
+                }
+            },
+            Message: {
+                validators: {
+                    notEmpty: {
+                        message: 'The Message is required and cannot be empty'
+                    }
+                }
+            }
+        }
+    });
